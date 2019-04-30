5d088c9cacb550c4b215a55f77abf92a328082ac
Tim Gibbon <tng@chegwin.org>
2019-04-30 16:31:23 +0100

    Protected date

--- dump_colours.py
+++ dump_colours.py
@@ -9,6 +9,6 @@ for key in (r.keys('*')):
     allkeys[key]=r.lrange(key, 0, -1)
 
 for lastkey in sorted(allkeys.keys()):
-    frmt_date = dt.datetime.utcfromtimestamp(float(lastkey)).strftime("%Y/%m/%d %H:%M:%S")
+    frmt_date = dt.datetime.utcfromtimestamp(float(lastkey)).strftime("[ %Y/%m/%d %H:%M:%S ]")
     #sys.stdout.write(frmt_date)
-    print lastkey, frmt_date , allkeys[ lastkey ]
+    print frmt_date , allkeys[ lastkey ]
